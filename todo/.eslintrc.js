44e79d1f40076b86375e55cb95e394e11d5bee0c
LaecioFillho <laeciofillho@gmail.com>
2024-04-03 22:30:42 -0300

    Todo 0.0.2
    Criando a forma da lista, e estilizando a cara do app.

--- todo/.eslintrc.js
+++ todo/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         es6: true,
         jest: true,
     },
-    extends: ['airbnb', 'prettier ', 'prettier/react'],
+    extends: ['airbnb', 'prettier', 'prettier/react'],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
+import { MdAutoDelete } from "react-icons/md";
 import './App.css';
 
 const App = () => {
     const ESCAPE_KEY = 27;
     const ENTER_KEY = 13;
 
-    const [value, setValue] = useState('');
+    const initialTodos = [
+        {id: 1, title: "Estudar React", checked: false},
+        {id: 2, title: "Estudar JavaScript", checked: false},
+        {id: 3, title: "Estudar TypeScript", checked: true},
+        {id: 4, title: "Estudar UI/UX", checked: false},
+    ];
+
+    const [todos,  ] = useState(initialTodos);
+    const [value, setValue] = useState("");
 
     const submit = () => {
         erase();
     }
 
-    const  erase = () => {
-        setValue(" ");
+    const erase = () => {
+        setValue("");
     }
 
     const onKeyDown = (event) => {
@@ -40,6 +49,14 @@ const App = () => {
                     onChange={onChange}
                     onKeyDown={onKeyDown}>
                 </input>
+                <ul className="todo-list">
+                    {todos.map((todo) => (
+                        <li key={todo.id.toString()}>
+                        <span className="todo">{todo.title}</span>
+                        <button className="remove"><MdAutoDelete size={28}/></button>
+                        </li>  
+                    ))}
+                </ul>
             </section>
         </section>
     );
